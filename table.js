4dd1b84fe49c9e8f62ff957e997ff59fbb4a9e00
Ryan Goniwiecha <rygon@umich.edu>
2020-05-08 00:24:24 -0400

    style selection working

--- table.js
+++ table.js
@@ -1,16 +1,18 @@
 function test() {
   var data = document.getElementById('dataentry').value.split('\n');
-  console.log(data);
+  var style = document.getElementById('table-style').value;
+  drawTable(style);
+  console.log(style);
 }
 
-function drawTable() {
+function drawTable(style) {
   var stage = new createjs.Stage("table");
   //Draw Background
   var bg = new createjs.Shape();
   bg.graphics.beginFill("#065861").drawRect(0, 0, 850, 480);
   stage.addChild(bg);
   var queue = new createjs.LoadQueue();
-  queue.loadFile({src:"dHC.png", id:"image"});
+  queue.loadFile({src:"static/images/dHC.png", id:"image"});
   queue.on("complete", (event) => {
 	var image = queue.getResult("image");
 	var bmp = new createjs.Bitmap(image);
@@ -21,26 +23,70 @@ function drawTable() {
 	stage.update();
   });
 
+//Draw Middle Divider
+  var mid = new createjs.Shape();
+  mid.graphics.beginFill("#ffffff").drawRect(227, 239, 396, 2);
+  stage.addChild(mid);
+  drawClanTag("ARC", "Arcadia Terra", stage, true);
+  drawClanTag("Bz", "Blizzard", stage, false);
+  drawARCLogo(stage, style);
+}
+
+//Draw clan tags
+function drawClanTag(tag, name, stage, win) {
   //Add Text
-  var ARC = new createjs.Text();
-  ARC.set({
-	text: "ARC",
+  let y_pos;
+  win ? y_pos = 55 : y_pos = 310;
+  var tagText = new createjs.Text();
+  tagText.set({
+	text: tag,
 	font: "bold 100px Tahoma",
 	color: "#ffffff",
 	textAlign: "center",
 	textBaseLine: "middle",
 	x: 155,
-	y: 60
+	y: y_pos
   });
-  ARC.shadow = new createjs.Shadow("#000000", 2, 2, 2);
-  //Middle Divider
-  var mid = new createjs.Shape();
-  mid.graphics.beginFill("#ffffff").drawRect(227, 239, 396, 2);
-  // mid.beginFill("#ffffff");
-  stage.addChild(ARC, mid);
+  var nameText = new createjs.Text();
+  nameText.set({
+	text: name,
+	font: "22px Oswald",
+	color: "#ffffff",
+	textAlign: "center",
+	textBaseLine: "middle",
+	x: 155,
+	y: y_pos + 98
+  });
+  tagText.shadow = new createjs.Shadow("#000000", 2, 2, 2);
+  nameText.shadow = new createjs.Shadow("#000000", 2, 2, 2);
+  stage.addChild(tagText);
+  stage.addChild(nameText);
+  stage.update();
 }
 
-drawTable();
-
-
-//color=065861
+//Draw ARC logo
+function drawARCLogo(stage, style) {
+  let filename;
+  switch(style) {
+  case "arc":
+	filename = "arc1_web.png"
+	break;
+  case "sky":
+	filename = "arcsky_web.png"
+	break;
+  case "terra":
+	filename = "arcterra_web.png"
+	break;
+  }
+  console.log(filename);
+  var queue = new createjs.LoadQueue();
+  queue.loadFile({src:"static/images/" + filename, id:"image"});
+  queue.on("complete", (event) => {
+	var image = queue.getResult("image");
+	var bmp = new createjs.Bitmap(image);
+	bmp.x = 117;
+	bmp.y = 207;
+	stage.addChild(bmp);
+	stage.update();
+  });
+}
